369a5963c506ddeb8362b2b3b6f4510c5fe1ec3e
DW <6@>
2023-08-16 20:57:07 +0800

    添加项目文件。

---
+++ Form.cs
@@ -0,0 +1,10 @@
+namespace IceRailHelper
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
