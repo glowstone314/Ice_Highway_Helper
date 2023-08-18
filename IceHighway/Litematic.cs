4bb0a4c29894a9196bc07ca0b428f22b928fe89b
DW <6@>
2023-08-18 22:46:15 +0800

    修改

---
+++ IceHighway/Litematic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ice_Highway_Helper.IceHighway
+{
+    public class Litematic
+    {
+
+        string author, description, name;
+        
+
+    }
+}
